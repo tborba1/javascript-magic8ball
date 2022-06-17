9eb97d6e1ea83d55052d8e39b09335e89eae4213
tborba1 <taryn.borba@gmail.com>
2022-06-17 15:36:30 -0700

    feat: updated css and js, removed images

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-//Select the element on the page to interact with!
-let button = document.querySelector("button");
-let answer = document.querySelector("#answer");
+let button = document.getElementById("button");
+let answer = document.getElementById("answer");
+let question = document.getElementById("question");
 
-// Add a click event to the button
 button.addEventListener("click", function () {
-      
-//Create array with answers
+      if (question.value.length < 1) {
+        alert("Enter a question!");
+      } else {
+
 const answers = [
     "It is certain",
     "It is decidedly so",
@@ -27,36 +28,10 @@ const answers = [
     "Better not tell you now",
     "Cannot predict now",
     "Seems silly"
-    // "Concentrate and ask again"
-    // '<img class="8ballRandom" src="img/magic8ball_1.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_2.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_3.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_4.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_5.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_6.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_7.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_8.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_9.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_10.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_11.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_12.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_13.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_14.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_15.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_16.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_17.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_18.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_19.png" />',
-    // '<img class="8ballRandom" src="img/magic8ball_20.png" />'
 ];
 
-//Generate a random number 
 const randomNumber = Math.random() * answers.length;
-
-//Turn Random number using the key above.
 const randomNumberFloor = Math.floor(randomNumber)
-
-//alert(answers[randomNumberFloor]);
 answer.innerHTML = answers[randomNumberFloor];
 
-});
+}});
